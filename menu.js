383fa55c2e3ea6a587ec3466cd367af2baf18193
Breanna B <breannacbai@gmail.com>
2024-07-17 15:40:59 -0400

    Shortened menu item descriptions

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 let menuItems = [{
   name: "Buttermilk Pancakes",
   price: 15.99,
-  desc: "Life isn't always beautiful. That was a lesson that Dan was learning. He also realized that life wasn't easy. This had come as a shock since he had lived a charmed life. He hated that this was the truth and he struggled to be happy knowing that his assumptions weren't correct. He wouldn't realize until much later in life that the difficult obstacles he was facing that were taking away the beauty in his life at this moment would ultimately make his life much more beautiful. All he knew was that at this moment was that life isn't always beautiful.",
+  desc: "Life isn't always beautiful. That was a lesson that Dan was learning. He also realized that life wasn't easy. This had come as a shock since he had lived a charmed life. He hated that this was the truth and he struggled to be happy knowing that his assumptions weren't correct.",
   image: "images/pancakes.jpg"
 }, {
   name: "Diner Double",
@@ -16,12 +16,12 @@ let menuItems = [{
 }, {
   name: "Egg Attack",
   price: 20.99,
-  desc: "Delicious Burger that contains an egg, hence the name \"egg\" attack. She never liked cleaning the sink. It was beyond her comprehension how it got so dirty so quickly. It seemed that she was forced to clean it every other day. Even when she was extra careful to keep things clean and orderly, it still ended up looking like a mess in a couple of days. What she didn't know was there was a tiny creature living in it that didn't like things neat.",
+  desc: "Delicious Burger that contains an egg, hence the name \"egg\" attack. She never liked cleaning the sink. It was beyond her comprehension how it got so dirty so quickly. It seemed that she was forced to clean it every other day.",
   image: "images/egg-attack.jpg"
 }, {
   name: "Oreo Dream",
   price: 20.99,
-  desc: "The Oreo dish of your dreams! The bridge spanning a 100-foot gully stood in front of him as the last obstacle blocking him from reaching his destination. While people may have called it a bridge, the reality was it was nothing more than splintered wooden planks held together by rotting ropes. It was questionable whether it would hold the weight of a child, let alone the weight of a grown man. The problem was there was no other way across the gully, and this played into his calculations of whether or not it was worth the risk of trying to cross it.",
+  desc: "The Oreo dish of your dreams! The bridge spanning a 100-foot gully stood in front of him as the last obstacle blocking him from reaching his destination. While people may have called it a bridge, the reality was it was nothing more than splintered wooden planks held together by rotting ropes.",
   image: "images/oreo-dream.jpg"
 }];
 
